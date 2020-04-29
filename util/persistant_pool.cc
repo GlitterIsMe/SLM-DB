12f69b2fcae4bc1f708406d8877d188279f3a707
zhangyiwen <zzyyyww9595@gmail.com>
2020-04-30 00:30:10 +0800

    convert libpmem to more suitable libvmem and modify dockerfile

--- util/persistant_pool.cc
+++ util/persistant_pool.cc
@@ -1,6 +1,6 @@
 #include "leveldb/persistant_pool.h"
 
-#include <libpmem.h>
+#include <libvmem.h>
 #include <unistd.h>
 
 namespace leveldb {
@@ -8,50 +8,36 @@ namespace nvram {
 
 #define LAYOUT_NAME "PMINDEXDB"
 
-static void* pm_pool;
+static VMEM* pm_pool;
 static bool init = false;
 static uint64_t allocs = 0;
-static size_t mapped_len = 0;
-static int is_pmem = 0;
-static uint64_t cur = 0;
-static uint64_t used = 0;
-
 
 void create_pool(const std::string& dir, const size_t& s) {
-  //size_t size = (s < PMEMCTO_MIN_POOL) ? PMEMCTO_MIN_POOL : s;
+  size_t pool_size = (s < VMEM_MIN_POOL) ? VMEM_MIN_POOL : s;
   size_t mapped_size;
   printf("Creating NVM pool size of %lu\n", s);
-  //pm_pool = pmemcto_create(dir.data(), LAYOUT_NAME, size, 0666);
-  bool file_is_exists = access(dir.c_str(), F_OK) ? false : true;
-  if (file_is_exists) {
-      remove(dir.c_str());
-  }
-  pm_pool = pmem_map_file(dir.c_str(), s, PMEM_FILE_CREATE, 0666, &mapped_size, &is_pmem);
+  pm_pool = vmem_create(dir.c_str(), s);
   if (pm_pool == nullptr) {
     fprintf(stderr, "pmem create error\n");
     perror(dir.data());
     exit(1);
   }
-  mapped_len = mapped_size;
+  init = true;
 }
 
 void close_pool() {
-    fprintf(stdout, "pmem allocs %lu\n", allocs);
-    fprintf(stdout, "pmem used %lu\n", used);
   if (init) {
     fprintf(stdout, "pmem allocs %lu\n", allocs);
-    fprintf(stdout, "pmem used %lu\n", used);
-    //pmemcto_close(pm_pool);
-    pmem_unmap(pm_pool, mapped_len);
+    vmem_delete(pm_pool);
+
   }
 }
 
 void pfree(void* ptr) {
-    used -= sizeof(ptr);
   if (!init) {
     free(ptr);
   } else {
-    //pmemcto_free(pm_pool, ptr);
+    vmem_free(pm_pool, ptr);
   }
 }
 
@@ -62,25 +48,19 @@ void* pmalloc(size_t size) {
     ptr = malloc(size);
   } else {
     allocs++;
-    if(cur + size > mapped_len){
+    ptr = vmem_alloc(pm_pool, size);
+    if(ptr == nullptr){
         fprintf(stderr, "pmem malloc error 2 \n");
         perror("vmem_malloc");
         exit(1);
     }
-    ptr = pm_pool + cur;
-    cur = cur + size;
-    /*if ((ptr = pmemcto_malloc(pm_pool, size)) == nullptr) {
-      fprintf(stderr, "pmem malloc error 2 \n");
-      perror("vmem_malloc");
-      exit(1);
-    }*/
   }
   return ptr;
 }
 
 void stats() {
 //  char *msg;
-//  pmemcto_stats_print(vmem, msg);
+    vmem_stats_print(pm_pool);
 //  printf("%s\n", msg);
 }
 
