0932206c16826d35488727d94a53e3e5813059c1
zhangyiwen <zzyyyww9595@gmail.com>
2020-05-01 18:49:47 +0800

    fix some error

--- Docker/run_docker.sh
+++ Docker/run_docker.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -it -v /home/zhangyiwen:/source -v /pmem0:/pmem centos7-libpmem-simple /bin/bash
+# run on server
+docker run -m 4096M -it -v /home/zhangyiwen:/source -v /pmem0:/pmem centos7:libpmem-simple /bin/bash
+
+# run on local
+#docker run -m 4096M -it -v /Users/zhangyiwen/Documents/cpp/SLM-DB:/source -v /Users/zhangyiwen/Documents/cpp/SLM-test/:/pmem centos7:libpmem-simple /bin/bash
