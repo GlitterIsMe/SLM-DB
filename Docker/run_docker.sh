695743d2e36c1fc0612c5636a85b94942bb07587
zhangyiwen <zzyyyww9595@gmail.com>
2020-05-03 10:47:06 +0800

    fix a typo

--- Docker/run_docker.sh
+++ Docker/run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run on server
-docker run -m 4096M -it -v /home/zhangyiwen:/source -v /pmem0:/pmem centos7:SLMDB-evaluation /bin/bash
+docker run -m 4096M -it -v /home/zhangyiwen:/source -v /pmem0:/pmem centos7:SLM-evaluation /bin/bash
 
 # run on local
 #docker run -m 4096M -it -v /Users/zhangyiwen/Documents/cpp/SLM-DB:/source -v /Users/zhangyiwen/Documents/cpp/SLM-test/:/pmem centos7:libpmem-simple /bin/bash
